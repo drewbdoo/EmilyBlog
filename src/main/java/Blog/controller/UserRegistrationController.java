95afc9a66c784b10a80d6a67c66c52d2a3978439
Queen Chungus <maprg.nv@gmail.com>
2022-06-16 14:37:43 -0400

    combined parts from ozzy and emily

--- src/main/java/Blog/controller/UserRegistrationController.java
+++ src/main/java/Blog/controller/UserRegistrationController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 //@SessionAttributes("user")
